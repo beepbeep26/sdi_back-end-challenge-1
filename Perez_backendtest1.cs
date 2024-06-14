8491762b43a886119398ef2d4722adf7882b5c6e
Earl Joshua Perez <106750430+beepbeep26@users.noreply.github.com>
2024-06-14 20:20:28 +0800

    Add files via upload

---
+++ Perez_backendtest1.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Programmed by: Earl Joshua Perez for TMSPH Back-end Test");
+bool tryAgain = true;
+
+while (tryAgain)
+{
+    Console.Write("Please input number (seat): ");
+    int seats = int.Parse(Console.ReadLine());
+
+    // Given Car Rental prices Arrays
+    int[] seatCapacities = { 5, 10, 15 };
+    int[] prices = { 5000, 8000, 12000 };
+    string[] sizes = { "S", "M", "L" };
+
+    //Let the price be the max value and be replaced with the cheaper price through for loop
+    //for loop repeats 3 times to accommodate the 3 categories
+    int carNum = 0;
+    int cheapestPrice = int.MaxValue;
+    string size = "";
+
+    for (int i = 0; i < 3; i++)
+    {
+        int numberOfCars = (int)Math.Ceiling((double)seats / seatCapacities[i]);
+        int totalPrice = numberOfCars * prices[i];
+
+        if (totalPrice < cheapestPrice)
+        {
+            cheapestPrice = totalPrice;
+            size = sizes[i];
+            carNum = numberOfCars;
+        }
+    }
+
+    // Print results
+    Console.WriteLine($"{size} x {carNum}");
+    Console.WriteLine("Total = PHP " + cheapestPrice);
+
+    //Try Again
+    Console.WriteLine("Try Again?(Type Y if Yes. If not, press any key to exit...)");
+
+    var input = Console.ReadLine();
+
+    if (input == "Y" || input == "y")
+    {
+        tryAgain = true;
+    }
+    else
+    {
+        tryAgain = false;
+        Console.WriteLine("Thank you for using my program.");
+    }
+}
---
+++ Perez_backendtest_Bonus.cs
@@ -0,0 +1,52 @@
+﻿Console.WriteLine("Programmed by: Earl Joshua Perez for TMSPH Back-end Test");
+bool tryAgain = true;
+
+while (tryAgain)
+{
+    Console.Write("Please input number (seat): ");
+    int seats = int.Parse(Console.ReadLine());
+
+    // Given Car Rental prices Arrays
+    int[] seatCapacities = { 5, 9, 15 }; //changed the capacities
+    int[] prices = { 5000, 8000, 11000 }; //changed the prices of M and L
+    string[] sizes = { "S", "Med", "L" };
+
+    //Let the price be the max value and be replaced with the cheaper price through for loop
+    //for loop repeats 3 times to accommodate the 3 categories
+    int carNum = 0;
+    int cheapestPrice = int.MaxValue;
+    string size = "";
+
+    for (int i = 0; i < 3; i++)
+    {
+        int numberOfCars = (int)Math.Floor((double)seats / seatCapacities[i]);
+        int maxCapacity = numberOfCars * seatCapacities[i];
+        int totalPrice = numberOfCars * prices[i];
+
+        if (totalPrice < cheapestPrice && maxCapacity >= seats)
+        {
+            cheapestPrice = totalPrice;
+            size = sizes[i];
+            carNum = numberOfCars;
+        }
+    }
+
+    // Print results
+    Console.WriteLine($"{size} x {carNum}");
+    Console.WriteLine("Total = PHP " + cheapestPrice);
+
+    //Try Again
+    Console.WriteLine("Try Again?(Type Y if Yes. If not, press any key to exit...)");
+
+    var input = Console.ReadLine();
+
+    if (input == "Y" || input == "y")
+    {
+        tryAgain = true;
+    }
+    else
+    {
+        tryAgain = false;
+        Console.WriteLine("Thank you for using my program.");
+    }
+}
